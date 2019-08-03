82848bee4c0ca436c6dc9b47414a23fa6a356c65
Paulo Ricardo Busch <paulo.ricardo@forlogic.net>
2019-08-03 18:15:49 -0300

    Teste de unidade para padrão Prototype

---
+++ UnitTests/PrototypetTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Prototype;
+using UnitTests.Util;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class PrototypetTests
+    {
+        [TestMethod]
+        public void Prototype()
+        {
+            var prototype = null as IPrototype;
+            var clone1 = prototype.Clone();
+
+            Assert.AreEqual(prototype.Identifier, clone1.Identifier);
+
+            prototype.Identifier = Rand.NewIdentifier();
+            var clone2 = prototype.Clone();
+
+            Assert.AreNotEqual(prototype.Identifier, clone1.Identifier);
+            Assert.AreEqual(prototype.Identifier, clone2.Identifier);
+        }
+    }
+}
---
+++ UnitTests/Util/Rand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Util
+{
+    public class Rand
+    {
+        public static string NewIdentifier() { 
+            return Guid.NewGuid().ToString("N");
+        }
+    }
+}
