5ab46a136878a604386db8e39df31eaaafa099ef
Paulo Ricardo Busch <paulo.ricardo@forlogic.net>
2019-08-04 12:38:25 -0300

    Projeto Observable

---
+++ Observable/IObserved.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObservablePattern
+{
+    public interface IObserved<TypeValue>
+    {
+        void Subscribe(IObservable<TypeValue> observable);
+        void Unsubscribe(IObservable<TypeValue> observable);
+        void NotifyObservable(TypeValue newValue);
+    }
+}
