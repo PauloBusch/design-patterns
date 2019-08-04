51229ca3ee33e60a7a4b243476a03ee95d167e52
Paulo Ricardo Busch <paulo.ricardo@forlogic.net>
2019-08-04 11:15:21 -0300

    Projeto Adapter

---
+++ Adapter/IUSBAdapter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern
+{
+    public interface IUSBAdapter
+    {
+        EUSBState UsbState { get; }
+        void Connect();
+        void Disconnect();
+    }
+
+    public enum EUSBState {
+        CONNECTED,
+        DISCONNECTED
+    }
+}
