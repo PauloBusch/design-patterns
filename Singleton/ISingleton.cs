a5dbfca10ef069365f8b431349ecd75c9d9cfd48
Paulo Ricardo Busch <paulo.ricardo@forlogic.net>
2019-08-03 22:20:49 -0300

    Projeto Singleton

---
+++ Singleton/ISingleton.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public interface ISingleton<Type>
+    {
+        Type Instance { get; }
+    }
+}
