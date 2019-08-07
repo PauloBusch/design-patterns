f036e1b03cc71acc8327d11be619cca244dd8a93
Paulo Ricardo Busch <paulo.ricardo@forlogic.net>
2019-08-07 20:38:31 -0300

    Teste de unidade para o padrão Command

---
+++ UnitTests/CommandTests.cs
@@ -0,0 +1,55 @@
+﻿using CommandPattern;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class CommandTests
+    {
+        [TestMethod]
+        public void Command()
+        {
+            MockCommand mockFileByOpen = null;
+            MockCommand mockFileByClose = null;
+
+            ICommand commadOpen = null;
+            ICommand commadClose = null;
+
+            commadOpen.Execute();
+            commadClose.Execute();
+
+            Assert.IsTrue(mockFileByOpen.InvokedOpen);
+            Assert.IsTrue(mockFileByClose.InvokedClose);
+
+            Assert.IsFalse(mockFileByOpen.InvokedClose);
+            Assert.IsFalse(mockFileByClose.InvokedOpen);
+        }
+    }
+
+    public class MockCommand : IFile
+    {
+        public bool InvokedClose;
+        public bool InvokedOpen;
+
+        public MockCommand()
+        {
+            InvokedOpen = false;
+            InvokedClose = false;
+        }
+
+        public void Close()
+        {
+            InvokedClose = true;
+        }
+
+        public void Open(string path)
+        {
+            InvokedOpen = true;
+        }
+    }
+}
